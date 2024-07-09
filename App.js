5b32d30101463d289bcaa3522d18ac814f8ea615
SF888888 <fang.su@northeastern.edu>
2024-07-09 13:43:14 -0700

    Added a button

--- App.js
+++ App.js
@@ -5,13 +5,16 @@ import Header from './Components/Header';
 export default function App() {
   const appName = 'Summer 2024 class';
   const[text, setText] = useState();
+  function handleInputData(){
+    console.log('call back', text);
+  }
   return (
     <View style={styles.container}>
       {/* use a prop to pass appName to Header */}
       <Header name={appName} >
         <Text>Header children</Text>
       </Header>
-      <Input/>
+      <Input inputHandler={handleInputData}/>
       <StatusBar style="auto" />
     </View>
   );
--- Components/Input.js
+++ Components/Input.js
@@ -1,9 +1,12 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import React, { useState, useEffect, useRef } from 'react'
 
 const Input = () => {
     const[text, setText] = useState('');
     const [isSubmitted, setIsSubmitted] = useState(false);
+    function handleConfirm(){
+        console.log('Submitted:', text);
+    }
     const inputRef = useRef(null);
     // Focus the TextInput when the component mounts
     useEffect(() => {
@@ -21,6 +24,7 @@ const Input = () => {
         />
         <Text>{text}</Text>
         {isSubmitted && text && <Text>Thank you</Text>}
+        <Button title = "Submit" onPress={() => {handleConfirm();}} />
         </View>
     )
 }
