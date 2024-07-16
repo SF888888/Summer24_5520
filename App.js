2e8fd433210915811a41b39d441a93bf3585fcf4
SF888888 <fang.su@northeastern.edu>
2024-07-16 14:08:34 -0700

    Defined navigation style

--- App.js
+++ App.js
@@ -11,7 +11,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} options={{
+          title: 'All Goals', 
+          headerStyle:{backgroundColor:'darkmagenta'}, 
+          headerTintColor:'white'}}/>
         <Stack.Screen name="GoalDetails" component={GoalDetails} />
       </Stack.Navigator>
     </NavigationContainer>
