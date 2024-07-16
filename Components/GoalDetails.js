0c2b966d2bf537f6ef647f1ca30686cddb1caebe
SF888888 <fang.su@northeastern.edu>
2024-07-16 14:26:31 -0700

    Added a more details button

--- Components/GoalDetails.js
+++ Components/GoalDetails.js
@@ -6,6 +6,7 @@ const GoalDetails = ({navigation, route}) => {
   return (
     <View>
       <Text>The details fo the goal with text:{route.params.goalObj.text} and id:{route.params.goalObj.id}</Text>
+    <Button title = 'More details' onpress={()=>{navigation.push("GoalDetails")}}></Button>
     </View>
   )
 }
