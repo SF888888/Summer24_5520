65c87484191268c15ce1e93effd25d24982bd6f7
SF888888 <fang.su@northeastern.edu>
2024-07-30 14:19:40 -0700

    Completed logic of Login

--- Components/Login.js
+++ Components/Login.js
@@ -1,13 +1,31 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function Login ({navigation}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLogin = async () => {};
+    const handleLogin = async () => {
+        if (!email.length){
+            Alert.alert('Please enter email address');
+            return;
+        }
+        if (!password.length){
+            Alert.alert('Please enter password');
+    
+            return;
+        }
+        try{
+            const userCred = await signInWithEmailAndPassword(auth, email, password)
+            console.log(userCred);
+        }
+        catch(err){
+            console.log(err);
+        }
+    };
     const handleSignup = async () => {
-        navigation.navigate('Signup');
+        navigation.replace('Signup');
     }
     return (
       <View style={styles.container}>
